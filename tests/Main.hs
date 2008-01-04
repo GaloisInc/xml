2b2f8391473462567ae00093d5344ab292984953
Don Stewart <dons@galois.com>
2008-01-04 15:41:13 -0800

    update tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -6,7 +6,7 @@ main = do
                 Nothing -> error "Failed to parse xml"
                 Just doc  -> 
                         let _:_:elem:_ = findElements (unqual "description") $ doc
-                            Just [Text title_content] = elContent elem
+                            [Text title_content] = elContent elem
                             title_text = cdData title_content
                         in mapM_ putStrLn $ lines title_text
 
