2da248919b893c8e47630b7ea251b579a91fca96
Magnus Carlsson <magnus@galois.com>
2008-02-15 13:11:07 -0800

    * Changed node to have type QName -> t -> Element.  * Added unode for making nodes with unqualified names.

--- Text/XML/Light.hs
+++ Text/XML/Light.hs
@@ -52,41 +52,45 @@ unqual x = blank_name { qName = x }
 -- | A smart element constructor which uses the type of its argument
 -- to determine what sort of element to make.
 class Node t where
-  node :: t -> Element
+  node :: QName -> t -> Element
 
-instance Node (QName,[Attr],[Content]) where
-  node (name,attrs,cont) = blank_element { elName     = name
-                                         , elAttribs  = attrs
-                                         , elContent  = cont
-                                         }
+instance Node ([Attr],[Content]) where
+  node n (attrs,cont) = blank_element { elName     = n
+                                      , elAttribs  = attrs
+                                      , elContent  = cont
+                                      }
 
-instance Node (QName,[Attr])    where node (n,as) = node (n,as,[]::[Content])
-instance Node (QName,Attr)            where node (n,a)  = node (n,[a])
-instance Node QName                   where node n      = node (n,[]::[Attr])
+instance Node [Attr]             where node n as   = node n (as,[]::[Content])
+instance Node Attr               where node n a    = node n [a]
+instance Node ()                 where node n ()   = node n ([]::[Attr])
 
-instance Node (QName,[Content])       where node (n,cs) = node (n,[]::[Attr],cs)
-instance Node (QName,Content)         where node (n,c)  = node (n,[c])
-instance Node (QName,[Attr],Content)  where node (n,as,c) = node (n,as,[c])
-instance Node (QName,Attr,Content)    where node (n,a,c)  = node (n,[a],[c])
+instance Node [Content]          where node n cs     = node n ([]::[Attr],cs)
+instance Node Content            where node n c      = node n [c]
+instance Node ([Attr],Content)   where node n (as,c) = node n (as,[c])
+instance Node (Attr,Content)     where node n (a,c)  = node n ([a],[c])
 
-instance Node (QName,[Attr],[Element]) where
-  node (n,as,cs) = node (n,as,map Elem cs)
+instance Node ([Attr],[Element]) where
+  node n (as,cs) = node n (as,map Elem cs)
 
-instance Node (QName,[Attr],Element)  where node (n,as,c) = node (n,as,[c])
-instance Node (QName,Attr,Element)    where node (n,a,c)  = node (n,[a],c)
-instance Node (QName,[Element])       where node (n,es) = node (n,[]::[Attr],es)
-instance Node (QName,Element)         where node (n,e)  = node (n,[e])
+instance Node ([Attr],Element)   where node n (as,c) = node n (as,[c])
+instance Node (Attr,Element)     where node n (a,c)  = node n ([a],c)
+instance Node ([Element])        where node n es     = node n ([]::[Attr],es)
+instance Node (Element)          where node n e      = node n [e]
 
-instance Node (QName,[Attr],[CData]) where
-  node (n,as,cs) = node (n,as,map Text cs)
+instance Node ([Attr],[CData])   where
+  node n (as,cs) = node n (as,map Text cs)
 
-instance Node (QName,[Attr],CData)  where node (n,as,c) = node (n,as,[c])
-instance Node (QName,Attr,CData)    where node (n,a,c)  = node (n,[a],c)
-instance Node (QName,[CData])       where node (n,es)   = node (n,[]::[Attr],es)
-instance Node (QName,CData)         where node (n,e)    = node (n,[e])
+instance Node ([Attr],CData)     where node n (as,c) = node n (as,[c])
+instance Node (Attr,CData)       where node n (a,c)  = node n ([a],c)
+instance Node [CData]            where node n es     = node n ([]::[Attr],es)
+instance Node CData              where node n e      = node n [e]
 
-instance Node (QName,[Attr],String) where
-  node (n,as,t) = node (n,as,blank_cdata { cdData = t })
+instance Node ([Attr],String)    where
+  node n (as,t) = node n (as,blank_cdata { cdData = t })
 
-instance Node (QName,Attr,String)   where node (n,a,t)  = node (n,[a],t)
-instance Node (QName,String)        where node (n,t)    = node (n,[]::[Attr],t)
+instance Node (Attr,String)      where node n (a,t)  = node n ([a],t)
+instance Node [Char]             where node n t      = node n ([]::[Attr],t)
+
+-- | Create node with unqualified name
+unode :: Node t => String -> t -> Element
+unode = node . unqual
